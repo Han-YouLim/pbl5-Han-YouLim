9a1ca88e2b44dea2c2b07c9705fcd2a2f9f88485
yrhan <gksdbfla12@naver.com>
2022-11-29 00:01:48 +0900

    [refact]

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const mysql = require('mysql');
 const fs = require('fs')
 const app = express()
 
-app.use(express.static(__dirname + "/"));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, 'clientsrc/build/index.html'));
-});
-
-
 const connection = mysql.createConnection({
     host: '127.0.0.1', // 여기 수정함
     user: 'root',
