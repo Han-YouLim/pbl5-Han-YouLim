8e26a60b6eac7a0ef6758b90559be1791463dd9f
yrhan <gksdbfla12@naver.com>
2022-12-01 11:52:46 +0900

    [feat] rm git cache &  add python modules

---
+++ pythonDir/usingClova.py
@@ -0,0 +1,114 @@
+import requests
+import json
+import pandas as pd
+import sys
+from env_list import CLOVA_INVOKE_URL, CLOVA_SECRET_KEY
+
+class ClovaSpeechClient:
+    invoke_url = CLOVA_INVOKE_URL
+    secret = CLOVA_SECRET_KEY
+
+    def req_url(self, url, completion, callback=None, userdata=None, forbiddens=None, boostings=None, wordAlignment=True, fullText=True, diarization=None):
+        request_body = {
+            'url': url,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/url',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_object_storage(self, data_key, completion, callback=None, userdata=None, forbiddens=None, boostings=None,
+                           wordAlignment=True, fullText=True, diarization=None):
+        request_body = {
+            'dataKey': data_key,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/object-storage',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_upload(self, file, completion, callback=None, userdata=None, forbiddens=None, boostings=None,
+                   wordAlignment=True, fullText=True, diarization=None):
+        request_body = {
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        print(json.dumps(request_body, ensure_ascii=False).encode('UTF-8'))
+
+        files = {
+            'media': open(file, 'rb'),
+            'params': (None, json.dumps(request_body, ensure_ascii=False).encode('UTF-8'), 'application/json')
+        }
+        response = requests.post(headers=headers, url=self.invoke_url + '/recognizer/upload', files=files)
+        return response
+
+def main():
+    global targetloc
+
+    if len(sys.argv) < 3:
+       targetloc = sys.argv[1]
+
+    res = ClovaSpeechClient().req_upload(file=targetloc, completion='sync')
+
+    with open('clovatest.json', 'w', encoding='UTF-8') as f:
+        f.write(res.text)
+
+    with open("clovatest.json", "r", encoding="utf8") as f:
+        contents = f.read() # string
+
+    json_d = json.loads(res.text)
+    sentence = []
+    start = []
+    end = []
+
+    for i in range(0, len(json_d["segments"])):
+        sentence.append(json_d["segments"][i]['text'])
+        start.append(json_d["segments"][i]['start'])
+        end.append(json_d["segments"][i]['end'])
+
+    df = pd.DataFrame({'sentence':sentence,
+                 'start' : start,
+                 'end' : end})
+
+    # df.to_csv('./csvData/STTResult_'+ targetname+'.csv', ',' ,index=False)
+    df.to_csv('./csvData/STTResult.csv', ',' ,index=False)
+
+if __name__ == '__main__':
+    main()
--- server.js
+++ server.js
@@ -23,6 +23,8 @@ connection.end();
 
 //file get
 const multer = require('multer');
+const PythonShell = require("python-shell");
+const {stringify} = require("nodemon/lib/utils");
 //in front user send their video to "/api/upload"
 //in back computer send processed video to "/upload"
 app.use("/", express.static('./inputData'))
@@ -37,17 +39,94 @@ let storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage }).single("file"); //파일하나만업로드하겠다는의미
 app.post("/api/upload", (req, res) => {
+    let fileName
     //비디오를 서버에 저장한다.
     upload(req, res, (err) => {
         if (err) {
             return res.json({ success: false, err });
         }
+        fileName = res.req.file.filename
         return res.json({
             success: true,
             url: res.req.file.path, //파일을 저장하게되면 uploads폴더 안에 저장되게되는데 그경로를 보내줌
             fileName: res.req.file.filename,
         });
     });
+
+    if(fileName){
+        const targetVideoFile = '/inputData/'+ fileName
+        //run python file here
+        let PythonShell = require('python-shell');
+        let cozyErrorList = []
+        let options1 = {
+            mode: 'text',
+            pythonPath: '/Users/han-yulim/opt/anaconda3/bin/python',
+            pythonOptions: ['-u'],
+            scriptPath: '',
+            args: [targetVideoFile]
+        };
+        let options2 = {
+            mode: 'text',
+            pythonPath: '/Users/han-yulim/opt/anaconda3/bin/',
+            pythonOptions: ['-u'],
+            scriptPath: '',
+            args: []
+        };
+
+        //클로바 가동 python file 실행
+        PythonShell.PythonShell.run('./pythonDir/'+"usingClova.py", options1, function (err, results) {
+            if (err){
+                cozyErrorList.push(stringify(err))
+                throw err;
+            }
+            else{
+                console.log("Round1 SUCCESS@@")
+            }
+            console.log('ROUND 1 >>>>>>>>>>>>>>> results: %j',results );
+        });
+
+        //stt전처리
+        PythonShell.PythonShell.run('./pythonDir/'+"STTResultPrePro.py", options2, function (err, results) {
+            if (err){
+                cozyErrorList.push(stringify(err))
+                throw err;
+            }
+            else{
+                console.log("Round2 SUCCESS@@")
+            }
+            console.log('ROUND 2 >>>>>>>>>>>>>>> results: %j',results );
+        });
+
+        //모델가동
+        PythonShell.PythonShell.run('./pythonDir/'+"load_CNN_model.py", options2, function (err, results) {
+            if (err) {
+                cozyErrorList.push(stringify(err))
+                throw err;
+            }
+            else{
+                console.log("Round3 SUCCESS@@")
+            }
+            console.log('ROUND 3 >>>>>>>>>>>>>>> results: %j',results );
+        });
+
+        //비디오 재가공
+        PythonShell.PythonShell.run('./pythonDir/'+"makeVideo.py", options1, function (err, results) {
+            if (err){
+                cozyErrorList.push(stringify(err))
+                throw err;
+            }
+            else{
+                console.log("Round4 SUCCESS@@")
+            }
+            console.log('ROUND 4 >>>>>>>>>>>>>>> results: %j',results );
+        });
+
+        cozyErrorList.map((value, index) => {
+            console.log("error"+ index+ "~~~~~~>"+ value)
+        })
+    }
+
+
 });
 
 // app.use(express.static(path.join(__dirname, 'clientsrc/build')));
