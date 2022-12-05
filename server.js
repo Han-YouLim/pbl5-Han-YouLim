0454767b80a17bf5acdbeed002f060c9edcbb799
yrhan <gksdbfla12@naver.com>
2022-12-05 21:24:32 +0900

    [feat] hide path variables

--- server.js
+++ server.js
@@ -4,28 +4,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const childProcess = require("child_process");
-const pythonScript =
-    ["~/Desktop/pbl5//pythonDir/usingClova.py",
-        "~/Desktop/pbl5/pythonDir/STTResultPrePro.py",
-        "~/Desktop/pbl5/pythonDir/load_CNN_model.py",
-        "~/Desktop/pbl5/pythonDir/makeVideo.py"]
-const environmentName = 'mlp';
-// const mysql = require('mysql');
-// const connection = mysql.createConnection({
-//     host: '127.0.0.1', // 여기 수정함
-//     user: 'root',
-//     password: '0000',
-//     database: 'pbl19',
-//     port: '3306',
-// });
-// connection.connect();
-//
-// connection.query('SELECT * from upload_file', (error, rows, fields) => {
-//     if (error) throw error;
-//     console.log('Test SUCCESS>>>>> ', rows);
-// });
-//
-// connection.end();
+const env = require('./env')
+const environmentName = env.environmentName
+const pythonScriptArr = env.pythonScript
+const targetVideoDir = env.targetVideoDir
 app.use(cors())
 
 function randomNumber(){
@@ -35,20 +17,21 @@ function randomNumber(){
     }
     return parseInt(str)
 }
+
 function runPython(fileName, res){
     if(fileName){
         console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Run python")
-        const targetVideoFile = '~/Desktop/pbl5/inputData/'+ fileName
+        const targetVideoFile = targetVideoDir + fileName
         let result
 
         //run python file here
         const command = `cd ~ && 
             source ~/opt/anaconda3/etc/profile.d/conda.sh && 
             conda activate ${environmentName} &&
-            python3 ${pythonScript[0]} ${targetVideoFile} && 
-            python3 ${pythonScript[1]} &&
-            python3 ${pythonScript[2]} &&
-            python3 ${pythonScript[3]} ${targetVideoFile} ${fileName}`
+            python3 ${pythonScriptArr[0]} ${targetVideoFile} && 
+            python3 ${pythonScriptArr[1]} &&
+            python3 ${pythonScriptArr[2]} &&
+            python3 ${pythonScriptArr[3]} ${targetVideoFile} ${fileName}`
 
         try{
             const pythonProcess = childProcess.spawnSync(command, { shell: true });
@@ -70,8 +53,6 @@ function runPython(fileName, res){
 
 //file get
 const multer = require('multer');
-//in front user send their video to "/api/upload"
-//in back computer send processed video to "/upload"
 app.use("/", express.static('./inputData'))
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
